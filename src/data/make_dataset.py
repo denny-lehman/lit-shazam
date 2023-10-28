6613525563b928ae5f59dfa59d8349a7444c227d
denny-lehman <den@berkeley.edu>
2023-10-28 00:18:19 -0600

    added remove_bookend func

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,3 +1,5 @@
+import requests
+
 def get_book(url:str):
     """Requests a book from project gutenberg and returns the string as text
     Args
@@ -19,3 +21,17 @@ def get_book(url:str):
         print(str(e))
         return None
         
+# all books in project gutenberg end with 
+# *** END OF THE PROJECT GUTENBERG EBOOK {Title} ***
+# *** END OF THE PROJECT GUTENBERG EBOOK THE GREAT GATSBY ***
+
+import re
+def remove_bookend(book:str)->str:
+    """removes the extra end of the book in project gutenberg"""
+    end_of_book_pattern = r'\*\*\* END OF THE PROJECT GUTENBERG EBOOK [\w\d\s]+ \*\*\*'
+    match = re.search(end_of_book_pattern, book)
+    if match is None:
+        print('could not find project gutenberg ending')
+        raise ValueError
+    last_character = match.start()
+    return book[:last_character]
